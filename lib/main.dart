7fb99ac6d2d457aa4902730049748927d49802e5
Too-Lazy-To-Code-It <2lazy2nameit@gmail.com>
2024-10-18 15:48:55 +0100

    Integrated UI 30%

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ArtStation Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: const Color(0xFF13AFF0), // ArtStation blue
+        scaffoldBackgroundColor: const Color(0xFF222222), // Dark background
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
       home: MainScreen(),
     );
@@ -27,8 +32,11 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
+  bool _isSidebarOpen = false;
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
 
   final List<Widget> _pages = [
     HomePage(),
@@ -39,18 +47,131 @@ class _MainScreenState extends State<MainScreen> {
     UserPage(),
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleSidebar() {
+    setState(() {
+      _isSidebarOpen = !_isSidebarOpen;
+      if (_isSidebarOpen) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    });
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+      if (_isSidebarOpen) {
+        _toggleSidebar();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text('ArtStation Clone'),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: _toggleSidebar,
+        ),
+      ),
+      body: Stack(
+        children: [
+          IndexedStack(
+            index: _selectedIndex,
+            children: _pages,
+          ),
+          if (_isSidebarOpen)
+            GestureDetector(
+              onTap: _toggleSidebar,
+              child: Container(
+                color: Colors.black54,
+              ),
+            ),
+          SlideTransition(
+            position: _slideAnimation,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Drawer(
+                child: Container(
+                  color: const Color(0xFF333333),
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      DrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        child: const Text(
+                          'ArtStation Clone',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.home),
+                        title: const Text('Home', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(0),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.shopping_bag),
+                        title: const Text('Shop', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(1),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.school),
+                        title: const Text('Learning', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(2),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.work),
+                        title: const Text('Job', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(3),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.event),
+                        title: const Text('Events', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(4),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.person),
+                        title: const Text('Profile', style: TextStyle(color: Colors.white)),
+                        onTap: () => _onItemTapped(5),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -62,10 +183,11 @@ class _MainScreenState extends State<MainScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
+        selectedItemColor: Theme.of(context).primaryColor,
         unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
         type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF333333),
       ),
     );
   }
