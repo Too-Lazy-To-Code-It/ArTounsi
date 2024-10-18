13fc5803c77ca041771ef2704d5309463cdd4c38
Too-Lazy-To-Code-It <2lazy2nameit@gmail.com>
2024-10-18 15:57:14 +0100

    Integrated UI 40%

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'pages/Post/home_page.dart';
-import 'pages/Shop/shop_page.dart';
-import 'pages/Learning/learning_page.dart';
-import 'pages/Job/job_page.dart';
-import 'pages/Event/events_page.dart';
-import 'pages/User/profile_page.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light,
+  ));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -14,11 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ArtStation Clone',
       theme: ThemeData(
-        primaryColor: const Color(0xFF13AFF0), // ArtStation blue
-        scaffoldBackgroundColor: const Color(0xFF222222), // Dark background
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.white),
-          bodyMedium: TextStyle(color: Colors.white70),
+        primaryColor: const Color(0xFF13AFF0),
+        scaffoldBackgroundColor: const Color(0xFF0A0A0A),
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme.apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ),
         ),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
@@ -34,160 +38,225 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
-  bool _isSidebarOpen = false;
-  late AnimationController _animationController;
-  late Animation<Offset> _slideAnimation;
-
-  final List<Widget> _pages = [
-    HomePage(),
-    ShopPage(),
-    LearningPage(),
-    JobPage(),
-    EventPage(),
-    UserPage(),
-  ];
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 250),
-    );
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(-1, 0),
-      end: Offset.zero,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeInOut,
-    ));
+    _tabController = TabController(length: 5, vsync: this);
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _tabController.dispose();
     super.dispose();
-  }
-
-  void _toggleSidebar() {
-    setState(() {
-      _isSidebarOpen = !_isSidebarOpen;
-      if (_isSidebarOpen) {
-        _animationController.forward();
-      } else {
-        _animationController.reverse();
-      }
-    });
   }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      if (_isSidebarOpen) {
-        _toggleSidebar();
-      }
+      _tabController.animateTo(index);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text('ArtStation Clone'),
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: _toggleSidebar,
+      body: SafeArea(
+        child: Column(
+          children: [
+            _buildAppBar(),
+            Expanded(
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  _buildHomePage(),
+                  _buildCommunityPage(),
+                  _buildSearchPage(),
+                  _buildNotificationsPage(),
+                  _buildMorePage(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-      body: Stack(
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IndexedStack(
-            index: _selectedIndex,
-            children: _pages,
+          Image.network(
+            'https://www.artstation.com/assets/logo-icon-white-5ba3bd87a12bc82c5a078c252e1f2f7f.png',
+            height: 32,
           ),
-          if (_isSidebarOpen)
-            GestureDetector(
-              onTap: _toggleSidebar,
-              child: Container(
-                color: Colors.black54,
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.search, color: Colors.white),
+                onPressed: () {},
               ),
-            ),
-          SlideTransition(
-            position: _slideAnimation,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.7,
-              child: Drawer(
-                child: Container(
-                  color: const Color(0xFF333333),
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: <Widget>[
-                      DrawerHeader(
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        child: const Text(
-                          'ArtStation Clone',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                          ),
-                        ),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.home),
-                        title: const Text('Home', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(0),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.shopping_bag),
-                        title: const Text('Shop', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(1),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.school),
-                        title: const Text('Learning', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(2),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.work),
-                        title: const Text('Job', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(3),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.event),
-                        title: const Text('Events', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(4),
-                      ),
-                      ListTile(
-                        leading: const Icon(Icons.person),
-                        title: const Text('Profile', style: TextStyle(color: Colors.white)),
-                        onTap: () => _onItemTapped(5),
-                      ),
-                    ],
-                  ),
-                ),
+              IconButton(
+                icon: const Icon(Icons.send, color: Colors.white),
+                onPressed: () {},
               ),
-            ),
+            ],
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Shop'),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'Learning'),
-          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Job'),
-          BottomNavigationBarItem(icon: Icon(Icons.event), label: 'Events'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+    );
+  }
+
+  Widget _buildHomePage() {
+    return ListView(
+      children: [
+        _buildStorySection(),
+        _buildFeedItem(),
+        _buildFeedItem(),
+      ],
+    );
+  }
+
+  Widget _buildStorySection() {
+    return Container(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF13AFF0), Color(0xFFFF00FF)],
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(2.0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage('https://picsum.photos/200'),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  'User $index',
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildFeedItem() {
+    return Card(
+      color: const Color(0xFF1A1A1A),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              backgroundImage: NetworkImage('https://picsum.photos/200'),
+            ),
+            title: const Text('Artist Name'),
+            subtitle: const Text('2 hours ago'),
+            trailing: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ),
+          Image.network(
+            'https://picsum.photos/400/300',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.favorite_border),
+                    SizedBox(width: 8),
+                    Icon(Icons.chat_bubble_outline),
+                    SizedBox(width: 8),
+                    Icon(Icons.send),
+                  ],
+                ),
+                Icon(Icons.bookmark_border),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Liked by User1 and 1,234 others',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('Artist caption goes here...'),
+          ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.grey,
+      ),
+    );
+  }
+
+  Widget _buildCommunityPage() {
+    return const Center(child: Text('Community Page'));
+  }
+
+  Widget _buildSearchPage() {
+    return const Center(child: Text('Search Page'));
+  }
+
+  Widget _buildNotificationsPage() {
+    return const Center(child: Text('Notifications Page'));
+  }
+
+  Widget _buildMorePage() {
+    return const Center(child: Text('More Page'));
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color(0xFF1A1A1A),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: TabBar(
+        controller: _tabController,
+        indicatorColor: Colors.transparent,
+        tabs: [
+          Tab(icon: Icon(Icons.home, color: _selectedIndex == 0 ? Theme.of(context).primaryColor : Colors.grey)),
+          Tab(icon: Icon(Icons.people, color: _selectedIndex == 1 ? Theme.of(context).primaryColor : Colors.grey)),
+          Tab(icon: Icon(Icons.search, color: _selectedIndex == 2 ? Theme.of(context).primaryColor : Colors.grey)),
+          Tab(icon: Icon(Icons.notifications, color: _selectedIndex == 3 ? Theme.of(context).primaryColor : Colors.grey)),
+          Tab(icon: Icon(Icons.menu, color: _selectedIndex == 4 ? Theme.of(context).primaryColor : Colors.grey)),
+        ],
         onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: const Color(0xFF333333),
       ),
     );
   }
