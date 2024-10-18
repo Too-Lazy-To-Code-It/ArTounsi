6419af8c4e022d9bf1cb197d460ff43a46f2a53b
Too-Lazy-To-Code-It <2lazy2nameit@gmail.com>
2024-10-18 16:10:40 +0100

    Integrated UI 50%

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,9 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
   late TabController _tabController;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final List<String> _pageTitles = ['Home', 'Community', 'Search', 'Notifications', 'More'];
 
   @override
   void initState() {
@@ -62,177 +65,79 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            _buildAppBar(),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  _buildHomePage(),
-                  _buildCommunityPage(),
-                  _buildSearchPage(),
-                  _buildNotificationsPage(),
-                  _buildMorePage(),
-                ],
-              ),
-            ),
-          ],
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1A1A1A),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
         ),
+        title: Text(_pageTitles[_selectedIndex]),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: _buildSidebar(),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildPage('Home'),
+          _buildPage('Community'),
+          _buildPage('Search'),
+          _buildPage('Notifications'),
+          _buildPage('More'),
+        ],
       ),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
-  Widget _buildAppBar() {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Image.network(
-            'https://www.artstation.com/assets/logo-icon-white-5ba3bd87a12bc82c5a078c252e1f2f7f.png',
-            height: 32,
-          ),
-          Row(
-            children: [
-              IconButton(
-                icon: const Icon(Icons.search, color: Colors.white),
-                onPressed: () {},
+  Widget _buildSidebar() {
+    return Drawer(
+      child: Container(
+        color: const Color(0xFF1A1A1A),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
               ),
-              IconButton(
-                icon: const Icon(Icons.send, color: Colors.white),
-                onPressed: () {},
+              child: const Text(
+                'ArtStation',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
               ),
-            ],
-          ),
-        ],
+            ),
+            _buildDrawerItem(Icons.home, 'Home', 0),
+            _buildDrawerItem(Icons.people, 'Community', 1),
+            _buildDrawerItem(Icons.search, 'Search', 2),
+            _buildDrawerItem(Icons.notifications, 'Notifications', 3),
+            _buildDrawerItem(Icons.more_horiz, 'More', 4),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildHomePage() {
-    return ListView(
-      children: [
-        _buildStorySection(),
-        _buildFeedItem(),
-        _buildFeedItem(),
-      ],
+  Widget _buildDrawerItem(IconData icon, String title, int index) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(title, style: const TextStyle(color: Colors.white)),
+      onTap: () {
+        _onItemTapped(index);
+        Navigator.pop(context);
+      },
     );
-  }
-
-  Widget _buildStorySection() {
-    return Container(
-      height: 100,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Container(
-                  width: 60,
-                  height: 60,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: [Color(0xFF13AFF0), Color(0xFFFF00FF)],
-                    ),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(2.0),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage('https://picsum.photos/200'),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  'User $index',
-                  style: const TextStyle(fontSize: 12),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildFeedItem() {
-    return Card(
-      color: const Color(0xFF1A1A1A),
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              backgroundImage: NetworkImage('https://picsum.photos/200'),
-            ),
-            title: const Text('Artist Name'),
-            subtitle: const Text('2 hours ago'),
-            trailing: IconButton(
-              icon: const Icon(Icons.more_vert),
-              onPressed: () {},
-            ),
-          ),
-          Image.network(
-            'https://picsum.photos/400/300',
-            fit: BoxFit.cover,
-            width: double.infinity,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.favorite_border),
-                    SizedBox(width: 8),
-                    Icon(Icons.chat_bubble_outline),
-                    SizedBox(width: 8),
-                    Icon(Icons.send),
-                  ],
-                ),
-                Icon(Icons.bookmark_border),
-              ],
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              'Liked by User1 and 1,234 others',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text('Artist caption goes here...'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCommunityPage() {
-    return const Center(child: Text('Community Page'));
-  }
-
-  Widget _buildSearchPage() {
-    return const Center(child: Text('Search Page'));
-  }
-
-  Widget _buildNotificationsPage() {
-    return const Center(child: Text('Notifications Page'));
-  }
-
-  Widget _buildMorePage() {
-    return const Center(child: Text('More Page'));
   }
 
   Widget _buildBottomNavigationBar() {
@@ -254,10 +159,16 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
           Tab(icon: Icon(Icons.people, color: _selectedIndex == 1 ? Theme.of(context).primaryColor : Colors.grey)),
           Tab(icon: Icon(Icons.search, color: _selectedIndex == 2 ? Theme.of(context).primaryColor : Colors.grey)),
           Tab(icon: Icon(Icons.notifications, color: _selectedIndex == 3 ? Theme.of(context).primaryColor : Colors.grey)),
-          Tab(icon: Icon(Icons.menu, color: _selectedIndex == 4 ? Theme.of(context).primaryColor : Colors.grey)),
+          Tab(icon: Icon(Icons.more_horiz, color: _selectedIndex == 4 ? Theme.of(context).primaryColor : Colors.grey)),
         ],
         onTap: _onItemTapped,
       ),
+    );
+  }
+
+  Widget _buildPage(String title) {
+    return Center(
+      child: Text(title, style: const TextStyle(fontSize: 24, color: Colors.white)),
     );
   }
 }
