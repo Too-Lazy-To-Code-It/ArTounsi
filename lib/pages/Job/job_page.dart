a42a637d4a451577edba0e79c0ebda5b95377050
rayenalmi <rayenalmi64@gmail.com>
2024-10-21 05:55:56 +0100

    Details Job

--- lib/pages/Job/job_page.dart
+++ lib/pages/Job/job_page.dart
@@ -1,7 +1,8 @@
-import 'package:Artounsi/entities/Job/project.dart';
 import 'package:Artounsi/pages/Job/project_creation_page.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+
+import '../../entities/Job/job.dart';
 
 class JobPage extends StatefulWidget {
   const JobPage({Key? key}) : super(key: key);
@@ -18,134 +19,219 @@ class _JobPageState extends State<JobPage> {
         10, (_) => chars.codeUnitAt(rnd.nextInt(chars.length))));
   }
 
-  late List<Project> projectList;
+  late List<Job> JobList;
 
   @override
   void initState() {
     super.initState();
-    projectList = [
-      Project(
+    JobList = [
+      Job(
         id: generateRandomId(),
         title: "Sunset Beach",
         description: "A beautiful oil painting of a sunset at the beach.",
         mainImagePath: "assets/images/cow.jpg",
         additionalImagePaths: [
-          "assets/images/sunset_beach_1.jpg",
-          "assets/images/sunset_beach_2.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/sunset-beach",
+        JobLink: "https://myportfolio.com/sunset-beach",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Urban Sketch",
         description: "A series of urban sketches capturing city life.",
         mainImagePath: "assets/images/child.jpeg",
         additionalImagePaths: [
-          "assets/images/urban_sketch_1.jpg",
-          "assets/images/urban_sketch_2.jpg",
-          "assets/images/urban_sketch_3.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/urban-sketch",
+        JobLink: "https://myportfolio.com/urban-sketch",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Abstract Emotions",
         description: "An abstract painting series exploring human emotions.",
         mainImagePath: "assets/images/marine.jpg",
         additionalImagePaths: [
-          "assets/images/abstract_emotions_1.jpg",
-          "assets/images/abstract_emotions_2.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/abstract-emotions",
+        JobLink: "https://myportfolio.com/abstract-emotions",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Nature Photography",
-        description:
-            "A collection of nature photographs from various national parks.",
+        description: "A collection of nature photographs from various national parks.",
         mainImagePath: "assets/images/Classy.jpg",
         additionalImagePaths: [
-          "assets/images/nature_photography_1.jpg",
-          "assets/images/nature_photography_2.jpg",
-          "assets/images/nature_photography_3.jpg",
-          "assets/images/nature_photography_4.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/nature-photography",
+        JobLink: "https://myportfolio.com/nature-photography",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Digital Art: Futuristic Cities",
-        description:
-            "A series of digital artworks depicting futuristic cityscapes.",
+        description: "A series of digital artworks depicting futuristic cityscapes.",
         mainImagePath: "assets/images/muslimFather.jpeg",
         additionalImagePaths: [
-          "assets/images/futuristic_cities_1.jpg",
-          "assets/images/futuristic_cities_2.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/futuristic-cities",
+        JobLink: "https://myportfolio.com/futuristic-cities",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Nature Photography",
-        description:
-            "A collection of nature photographs from various national parks.",
+        description: "A collection of nature photographs from various national parks.",
         mainImagePath: "assets/images/capital.jpeg",
         additionalImagePaths: [
-          "assets/images/nature_photography_1.jpg",
-          "assets/images/nature_photography_2.jpg",
-          "assets/images/nature_photography_3.jpg",
-          "assets/images/nature_photography_4.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/nature-photography",
+        JobLink: "https://myportfolio.com/nature-photography",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Game Card",
-        description:
-            "A collection of nature photographs from various national parks.",
+        description: "A collection of nature photographs from various national parks.",
         mainImagePath: "assets/images/cardGame.jpeg",
         additionalImagePaths: [
-          "assets/images/nature_photography_1.jpg",
-          "assets/images/nature_photography_2.jpg",
-          "assets/images/nature_photography_3.jpg",
-          "assets/images/nature_photography_4.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/nature-photography",
+        JobLink: "https://myportfolio.com/nature-photography",
       ),
-      Project(
+      Job(
         id: generateRandomId(),
         title: "Mosque",
-        description:
-            "A collection of nature photographs from various national parks.",
+        description: "A collection of nature photographs from various national parks.",
         mainImagePath: "assets/images/mos.jpeg",
         additionalImagePaths: [
-          "assets/images/nature_photography_1.jpg",
-          "assets/images/nature_photography_2.jpg",
-          "assets/images/nature_photography_3.jpg",
-          "assets/images/nature_photography_4.jpg",
+          "assets/images/marine.jpg",
+          "assets/images/muslimFather.jpeg",
+          "assets/images/cardGame.jpeg",
+          "assets/images/mos.jpeg",
         ],
-        projectLink: "https://myportfolio.com/nature-photography",
+        JobLink: "https://myportfolio.com/nature-photography",
       ),
     ];
+  }
+
+  void _showJobDetails(BuildContext context, Job Job) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Container(
+            constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.7),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      Job.title,
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 16),
+                    Image.asset(
+                      Job.mainImagePath,
+                      fit: BoxFit.cover,
+                      height: 200,
+                      width: double.infinity,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      Job.description,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "Additional Images:",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    Container(
+                      height: 100,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: Job.additionalImagePaths.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Image.asset(
+                              Job.additionalImagePaths[index],
+                              fit: BoxFit.cover,
+                              width: 100,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    InkWell(
+                      child: Text(
+                        "Job Link",
+                        style: TextStyle(fontSize: 16, color: Colors.blue, decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        // TODO: Implement link opening functionality
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        child: Text("Close"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*appBar: AppBar(
-        title: Text('My Portfolio'),
-      ),*/
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemCount: projectList.length,
+        itemCount: JobList.length,
         itemBuilder: (context, index) {
           return GestureDetector(
             onTap: () {
-              // Navigate to project detail page
+              _showJobDetails(context, JobList[index]);
             },
             child: Card(
               child: Padding(
@@ -154,13 +240,13 @@ class _JobPageState extends State<JobPage> {
                   children: [
                     Expanded(
                       child: Image.asset(
-                        projectList[index].mainImagePath,
+                        JobList[index].mainImagePath,
                         fit: BoxFit.cover,
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(projectList[index].title),
+                      child: Text(JobList[index].title),
                     ),
                   ],
                 ),
